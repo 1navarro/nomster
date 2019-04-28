b206cdf462c3ab5f6aa5acdafa63f921a61494b9
Juan <juannavarro182@gmail.com>
2019-04-28 19:05:53 +0000

    added username to database

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
   include Pagy::Backend
   Pagy::VARS[:items] = 9
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     #@places = Place.all
@@ -12,7 +13,7 @@ class PlacesController < ApplicationController
   end
 
   def create
-    Place.create(place_params)
+    current_user.places.create(place_params)
     redirect_to root_path
   end
 
